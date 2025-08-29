5b0c05a49b227729ef4c8d3dd5c81499dd01d449
Heloísa T. <heloisataraujo14@gmail.com>
2025-08-29 17:53:26 -0300

    Create Variaveis.js

---
+++ Variaveis.js
@@ -0,0 +1,33 @@
+let nome= "Helo"; //Váriavel de armazenamento de string 
+const idade= 19; //Váriavel de aramazenamento de Número
+var cidade= "São Bernardo"
+
+// const = variável que não pode ser alterada 
+// var e let= similares, podem ter seu vlor alterado (var esta sendo descontinuado)
+
+//Mostrar váriavel no terminal
+console.log(nome);
+
+//Mostrar váriaveis em uma frase
+console.log("Me chamo " + nome + ", tenho " + idade + " anos de idade" + " e sou de " + cidade); 
+
+console.log(`O meu nome é ${nome}, eu tenho ${idade} anos e nasci em ${cidade}`);
+
+//Váriavel Boolean que idica verdadeiro e/ou falso:
+
+let estaChovendo = false;
+let estaNublado = true;
+
+//Váriaveis undefined
+
+let marca; 
+console.log(marca);
+
+//Váriavel nula
+
+let endereco = null;
+
+//Mostrar tipo primitivo da váriavel
+//typeof
+console.log(typeof nome);
+
